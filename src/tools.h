50b8df56919cd29f692b7bf5b104b28bc218cd12
tomwitsenboer <40236346+tomwitsenboer@users.noreply.github.com>
2018-12-06 21:04:54 +0100

    Update tools.h

--- src/tools.h
+++ src/tools.h
@@ -2,11 +2,9 @@
 #define TOOLS_H_
 #include <vector>
 #include "Eigen/Dense"
-#include "Eigen/SVD"
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-using Eigen::Ref;
 using namespace std;
 
 class Tools {
@@ -22,14 +20,9 @@ public:
   virtual ~Tools();
 
   /**
-  * A helper method to calculate RMSE (root-mean-square error).
+  * A helper method to calculate RMSE.
   */
   VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
-
-  /**
-   * A helper method to normalize angle, i.e. make it belong to [-pi, pi].
-   */
-  void NormalizeAngle(double *angle);
 
 };
 
